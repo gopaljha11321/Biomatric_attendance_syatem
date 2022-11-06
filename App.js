f735a9bdd96f69215b2c7acefaff730f958d865b
Gopal Jha <82890881+gopaljha11321@users.noreply.github.com>
2022-11-07 00:24:51 +0530

    third commit
    third commit

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 const express = require('express');
-const {engine} = require('express-handlebars');
+var hbs = require('hbs')
+const mongo  = require('mongodb').MongoClient;
+const url = 'mongodb+srv://gopaljha:jhaji9871436400@cluster0.n70vgmg.mongodb.net/attendance_system?retryWrites=true&w=majority';
+const client= new mongo(url,(err)=>
+{
+    if(err) throw err;
+})
+const dbName = 'attendance_system';
 const app =express()
-const port =3000
+const port =process.env.PORT||3000
+const db = client.db(dbName);
+const collection =db.collection('students')
 let date_ob = new Date();
-// app.engine('handlebars', engine({ extname: '.hbs', defaultLayout: "views"}));
-app.engine('handlebars', engine({
-    layoutsDir: __dirname + '/views',
-    }));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs')
+app.set('views',"views")
+  
 app.get('/',(req,res)=>
 {
-    
-    res.render("index",{layout:"index"})
+    const info=[]
+    async function getData()
+  {
+    let data=[]
+    let result = await collection.find({ "name" : { $exists : true }}).toArray();
+    for(let i=0;i<result.length;i++)
+    {
+        data["name"]=result[i].name;
+        data["no"]=result[i].enrollmentNo;
+        data["date"]=result[i].date;
+        info.push(data);
+    }
+    console.log(info);
+    res.render("index",{info:info})
+  }
+   getData();
 })
 app.get('/set',(req,res)=>
 {
@@ -23,10 +44,32 @@ app.get('/set',(req,res)=>
     let minutes = date_ob.getMinutes();
     let seconds = date_ob.getSeconds();
     console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
-    console.log(hours + ":" + minutes);
-    return res.send("get_data")
+    const insertResult = collection.insertOne({"name":req.query.name,"enrollmentNo":req.query.no,date:(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds)});
+    return res.send("set_data")
 })
 app.listen(port,()=>
 {
-    console.log("app start");
+    console.log("server_start");
 })
+//mongodb+srv://gopaljha:jhaji9871436400@cluster0.n70vgmg.mongodb.net/project_ai?retryWrites=true&w=majority
+
+// const client= new mongo(url,(err)=>
+// {
+//     if(err) throw err;
+// })
+// client.connect()
+// console.log("connection done")
+// const db = client.db(dbName);
+// const collection =db.collection('Keywords')
+// const collection2 =db.collection('Answers')
+//insertion in atlas
+//  const insertResult = collection.insertOne({_id:252, name:"ram",
+// age: 19,mobile: "9871436400" });
+
+//deletion on atlas
+// collection.deleteMany()
+// collection2.deleteMany()
+
+//read from mongodb
+  
+// console.log("done delete")
